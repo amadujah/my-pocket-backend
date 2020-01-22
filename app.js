a719dc1d189d6e258147b0176accec1ee9b3f3d4
amad <=>
2020-01-22 20:40:03 +0100

    update dependencies

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 //recipe routes
 import recipe from './api/recipes/recipe.route';
-import spending from './api/recipes/spending.route'
+import spending from './api/spending/spending.route';
 
 
 const app = express()
@@ -10,7 +10,7 @@ const app = express()
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://localhost:27017/safe-pocket';
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB,  { useNewUrlParser: true });
+mongoose.connect(mongoDB,  { useNewUrlParser: true , useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/recipes', recipe);
-app.use('/spending', spending);
+// app.use('/spending', spending);
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`)
